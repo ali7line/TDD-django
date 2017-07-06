ee30984976b0b9babf8bcd4c3ed00d26982986a2
ali7line <soltani.110@gmail.com>
2017-07-06 23:51:09 +0430

    FT checks for added to-do list item

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,3 +1,4 @@
+from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 import unittest
 
@@ -16,11 +17,21 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
         # She notices the page title and header mention to-do lists
         self.assertEqual('To-Do List', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do List', header_text)
         # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual('Ender a to-do item', inputbox.get_attribute('placeholder'))
         # She types "Buy peacock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
         # The page updates again, and now shows both items on her list
