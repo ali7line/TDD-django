38cf73f0740108ed662a51c112fd7f9fdb74f037
ali7line <soltani.110@gmail.com>
2017-07-18 12:11:39 +0430

    refactor FT

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -11,6 +11,16 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, raw_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(raw_text, [row.text for row in rows])
+
+    def enter_text_into_list(self, raw_text):
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys(raw_text)
+        inputbox.send_keys(Keys.ENTER)
+
     def test_visitor_cresates_list_and_retrieves_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
@@ -22,18 +32,20 @@ class NewVisitorTest(unittest.TestCase):
         # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual('Enter a to-do item', inputbox.get_attribute('placeholder'))
+
         # She types "Buy peacock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
-        inputbox.send_keys('Buy peacock feathers')
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
-        inputbox.send_keys(Keys.ENTER)
+        self.enter_text_into_list('Buy peacock feathers')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+        self.enter_text_into_list('Use peacock feather to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feather to make a fly')
+
         # The page updates again, and now shows both items on her list
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
