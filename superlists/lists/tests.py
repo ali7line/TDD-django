5ef9955c2cd9aaffe828c61d8ed6bd15a66c94ee
ali7line <soltani.110@gmail.com>
2017-07-18 12:28:56 +0430

    adds Item model for lists and its migrations and tests

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -2,6 +2,7 @@ from django.core.urlresolvers import resolve
 from django.test import TestCase
 
 from .views import homepage
+from .models import Item
 
 
 class SmokeTest(TestCase):
@@ -17,3 +18,23 @@ class SmokeTest(TestCase):
         response = self.client.post('/', {'item_text': 'new item list'})
 
         self.assertIn('new item list', response.content.decode())
+
+
+class ItemListTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'the first item in list'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'the second item in list'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(Item.objects.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+
+        self.assertEqual(first_saved_item.text, 'the first item in list')
+        self.assertEqual(second_saved_item.text, 'the second item in list')
