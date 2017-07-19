63987c70ab7a16fdc0db2287981d1da8f70dcb75
ali7line <soltani.110@gmail.com>
2017-07-19 11:27:51 +0430

    updates FT to include multi use functionality of lists

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -39,18 +39,40 @@ class NewVisitorTest(LiveServerTestCase):
         # "1: Buy peacock feathers" as an item in a to-do list
         self.enter_text_into_list('Buy peacock feathers')
 
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
-        self.enter_text_into_list('Use peacock feather to make a fly')
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table('2: Use peacock feather to make a fly')
-
         # The page updates again, and now shows both items on her list
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
         # explanatory text to that effect.
         # She visits that URL - her to-do list is still there.
         # Satisfied, she goes back to sleep
+        self.enter_text_into_list('Use peacock feather to make a fly')
+        self.check_for_row_in_list_table('2: Use peacock feather to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.browser.quit()
+
+        # A new user , Francis, comes along to the site
+        # and sees a blank page asking him to enter new item
+        self.browser = webdriver.Chrome()
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+        # He adds an item and will be redirected to a new url (different from edith)
+        self.enter_text_into_list('Buy milk')
+
+        francis_list_url = self.browser.current_url
+        self.assertNotEqual(edith_list_url, francis_list_url)
+
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.check_for_row_in_list_table('1: Buy milk')
+
         self.fail('Finish Test!')
